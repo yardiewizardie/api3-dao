7bd818d0abc15bb547c49a97a51fce1ecfe39105
yardiewizardie <yardiewizardie@gmail.com>
2021-01-25 09:11:40 -0400

    modal

--- packages/ui/src/components/StakingContainer/index.tsx
+++ packages/ui/src/components/StakingContainer/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Typography } from '@material-ui/core';
+import React, { useState } from 'react';
+import { Typography, Modal, Paper, TextField } from '@material-ui/core';
 import { BasicButton } from "components"
 
 import useStyles from "components/StakingContainer/styles";
@@ -7,6 +7,27 @@ import useCommonStyles from "styles/common-styles";
 
 function StakingContainer() { 
     const commonClasses = useCommonStyles();
+    const [modal, setUnstakeModal] = useState(false);
+
+    const UnstakeModal = () => {
+        const [unstakeAmount, setUnstakeAmount] = useState(0);
+
+        return (
+        <Modal
+            open={modal}
+            onClose={() => setUnstakeModal(false)}
+            aria-labelledby="simple-modal-title"
+            aria-describedby="simple-modal-description"
+          >
+              <div className={commonClasses.centeredBox} style={{ height: "100%" }}>
+                <Paper style={{ height: "250px", width: "400px", padding: "0 2%" }}>
+                    <Typography variant="body1" color="primary" style={{ padding: "2%" }}>How many tokens would you like to unstake?</Typography>
+                    <TextField defaultValue={unstakeAmount} value={unstakeAmount} />
+                    <BasicButton title="Initiate Unstaking" />
+                </Paper>
+            </div>
+          </Modal>)
+    }
 
     return (<div className={commonClasses.marginContainer} style={{ width: "50%" }}>
             <div className={commonClasses.titleWithButton}>
@@ -21,7 +42,8 @@ function StakingContainer() {
                 <Typography variant="h2" color="secondary" style={{ padding: "2%" }}>0</Typography>
             </div>
             <div className={commonClasses.leftBox}>
-                <Typography variant="body1" color="secondary" style={{ textDecoration: "underline "}}>Initiate Unstake</Typography>
+                <Typography variant="body1" color="secondary" style={{ textDecoration: "underline "}} onClick={() => setUnstakeModal(true)}>Initiate Unstake</Typography>
+                {UnstakeModal()}
             </div>
       </div>
     </div>);
